eb8314b46c71ebd9bbfd3e680a638e9fe945f354
wuxb <wu.xiaobing1@zte.com.cn>
2017-12-25 20:58:56 +0800

    工具类

---
+++ src/main/java/utils/CommonUtils.java
@@ -0,0 +1,11 @@
+package utils;
+
+/**
+ * @Author: wuxiaobing
+ * @Date 2017/12/25
+ **/
+public class CommonUtils {
+    public static void print(final Object obj) {
+        System.out.println(obj);
+    }
+}
