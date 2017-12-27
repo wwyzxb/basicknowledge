4cb054d23680b31f8c94bc7a8e99f1f9ee5becbd
wwyzxb <wwyzxb@163.com>
2017-12-27 23:34:18 +0800

    hashMap测试

---
+++ src/main/java/javase/collection/HashMapTest.java
@@ -0,0 +1,33 @@
+package javase.collection;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author Vincent
+ * @Date 2017/12/11 23:10
+ */
+public class HashMapTest {
+
+    public static void main(String[] args){
+        Map<String, String> map = new HashMap<>();
+        String key = map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+        map.put("key4", "value4");
+        map.put("key5", "value5");
+        map.put("key6", "value6");
+        map.put("key7", "value7");
+        map.put("key8", "value8");
+        map.put("key9", "value9");
+        map.put("key10", "value10");
+        map.put("key11", "value11");
+        map.put("key12", "value12");
+        map.put("key13", "value13");
+        map.put("key14", "value14");
+        map.put("key15", "value15");
+        map.put("key16", "value16");
+        map.put("key17", "value17");
+    }
+}
