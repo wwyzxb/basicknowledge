692c71fc373c0452c9c8c110a827a530adc70a31
wuxb <wu.xiaobing1@zte.com.cn>
2018-01-21 16:27:19 +0800

    泛型test

--- src/main/java/json/gson/ResultUtils.java
+++ src/main/java/json/gson/ResultUtils.java
@@ -2,6 +2,8 @@ package json.gson;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.junit.Test;
+import utils.CommonUtils;
 
 import java.lang.reflect.Type;
 import java.util.List;
@@ -15,6 +17,7 @@ public class ResultUtils {
 
     /**
      * 将Result对象转换成json字符串
+     *
      * @param result
      * @return
      */
@@ -24,6 +27,7 @@ public class ResultUtils {
 
     /**
      * 将json字符串转换成相应的对象（这里的泛型类型为T）
+     *
      * @param json
      * @param clazz
      * @param <T>
@@ -37,15 +41,30 @@ public class ResultUtils {
 
     /**
      * 将Json字符串转换成相应的对象（这里的泛型类型为List）
+     *
      * @param json
      * @param clazz
      * @param <T>
      * @return
      */
-    public static <T> Result<List<T>> fromArrJson(String json,Class<T> clazz){
-        Type listType=new ParameterizedTypeImpl(List.class,null,new Class[]{clazz});
-        Type type=new ParameterizedTypeImpl(Result.class,null,new Type[]{listType});
-        Result<List<T>> result=GSON.fromJson(json,type);
+    public static <T> Result<List<T>> fromArrJson(String json, Class<T> clazz) {
+        Type listType = new ParameterizedTypeImpl(List.class, null, new Class[]{clazz});
+        Type type = new ParameterizedTypeImpl(Result.class, null, new Type[]{listType});
+        Result<List<T>> result = GSON.fromJson(json, type);
         return result;
+    }
+
+    public static <T> T fromJsonToObj(String json, Class<T> clazz) {
+        T obj = GSON.fromJson(json, clazz);
+        return obj;
+    }
+
+    @Test
+    public void testFromJsonToObj() {
+        StudentVO studentVO = StudentVO.builder().setName("Vincent").setAge(26).build();
+        String json = GSON.toJson(studentVO);
+        CommonUtils.print(json);
+        StudentVO obj = fromJsonToObj(json, StudentVO.class);
+        System.out.println(obj);
     }
 }
