89bf7c4d8ea41a8790942078dac821f80c7346e8
wuxb <wu.xiaobing1@zte.com.cn>
2018-01-21 16:26:24 +0800

    lambda test

---
+++ src/main/java/javase/java8/demo/FindHiddenFiles.java
@@ -0,0 +1,37 @@
+package javase.java8.demo;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ * @Author: wuxiaobing
+ * @Date 2018/1/20
+ **/
+public class FindHiddenFiles {
+    @Test
+    public void testHiddenFiles(){
+        File[] hiddenFiles=new File(".").listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isHidden();
+            }
+        });
+
+        for(File file:hiddenFiles){
+            System.out.println(file.getName());
+        }
+    }
+
+    @Test
+    public void testHiddenFilesInJava8(){
+        /**
+         * File::isHidden创建了一个方法引用
+         * */
+        File[] hiddenFiles=new File(".").listFiles(File::isHidden);
+        for(File file:hiddenFiles){
+            System.out.println(file.getName());
+        }
+    }
+}
---
+++ src/main/java/javase/java8/demo/PickApples.java
@@ -0,0 +1,66 @@
+package javase.java8.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * @Author: wuxiaobing
+ * @Date 2018/1/20
+ **/
+public class PickApples {
+    static List<Apple> inventory;
+    static {
+        inventory = Arrays.asList(new Apple("green", 160), new Apple("red", 160), new Apple("green", 150), new Apple("red", 150));
+
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (p.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 将方法作为参数进行传递
+     * */
+    @Test
+    public void testFilterApples() {
+        List<Apple> result =filterApples(inventory, Apple::isGreenApple);
+        result=filterApples(result, Apple::isHeavyApple);
+        System.out.println(result);
+    }
+
+    /**
+     * 使用Lambda来代替只会使用到一次的方法
+     * */
+    @Test
+    public void testFilterApplesWithLambda(){
+        List<Apple> result =filterApples(inventory, (Apple a)-> a.getWeight()>150&&"red".equals(a.getColor()));
+        System.out.println(result);
+    }
+}
+
+@Data
+@AllArgsConstructor
+class Apple {
+    private String color;
+    private int weight;
+
+    public static boolean isGreenApple(Apple apple) {
+        return "green".equals(apple.getColor());
+    }
+
+    public static boolean isHeavyApple(Apple apple) {
+        return apple.getWeight() > 150;
+    }
+}
