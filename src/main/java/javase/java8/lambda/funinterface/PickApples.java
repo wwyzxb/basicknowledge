61543bd07930cd6b17f8b04a3cf7fb09be729f84
wwyzxb <wxb910413>
2018-02-05 22:18:47 +0800

    LAMBDA

--- src/main/java/javase/java8/lambda/funinterface/PickApples.java
+++ src/main/java/javase/java8/lambda/funinterface/PickApples.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -13,16 +14,58 @@ import java.util.function.Predicate;
  * @Author: wuxiaobing
  * @Date 2018/1/20
  **/
+class AppleComparator implements Comparator<Apple> {
+
+    @Override
+    public int compare(Apple o1, Apple o2) {
+        return o1.getWeight().compareTo(o2.getWeight());
+    }
+}
+
 public class PickApples {
-    static List<Apple> inventory;
+    private final static List<Apple> inventory;
 
     static {
-        inventory = Arrays.asList(
-                new Apple("green", 160),
-                new Apple("red", 160),
-                new Apple("green", 150),
-                new Apple("red", 150));
+        inventory = Arrays.asList(new Apple("green", 160), new Apple("red", 140), new Apple("green", 150), new Apple("red", 149));
+    }
 
+    /**
+     * 代码传递
+     */
+    @Test
+    public void testSortInventory1() {
+        inventory.sort(new AppleComparator());
+    }
+
+    /**
+     * 匿名类
+     */
+    @Test
+    public void testSortInventory2() {
+        inventory.sort(new Comparator<Apple>() {
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return o1.getWeight().compareTo(o2.getWeight());
+            }
+        });
+    }
+
+    /**
+     * Lambda
+     */
+    @Test
+    public void testSortInventory3() {
+        inventory.sort((Apple o1, Apple o2) -> o1.getWeight().compareTo(o2.getWeight()));
+    }
+
+    /**
+     * 方法引用
+     */
+    @Test
+    public void testSortInventory4() {
+        inventory.sort(Comparator.comparing(Apple::getWeight));
+        //或者
+        inventory.sort(Comparator.comparing((Apple a)->a.getWeight()));
     }
 
     public List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
@@ -59,7 +102,7 @@ public class PickApples {
 @AllArgsConstructor
 class Apple {
     private String color;
-    private int weight;
+    private Integer weight;
 
     public static boolean isGreenApple(Apple apple) {
         return "green".equals(apple.getColor());
